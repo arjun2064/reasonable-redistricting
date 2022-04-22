7620b2d95f86ea67412c57cb2dc68f8ae12b0d61
hhhzzzsss <hyzhou42@gmail.com>
2022-04-22 00:50:16 +0000

    Fixed graph.h public/private

--- Graph.h
+++ Graph.h
@@ -3,17 +3,19 @@
 
 using std::vector;
 class Graph {
-  /*
-      nodes are uniquely identified by id/number
-      precincts, being vertexes, connected by edges
-  */
-  vector<Precinct> precincts;
+    public:
+        
+    private:
+        /*
+            nodes are uniquely identified by id/number
+            precincts, being vertexes, connected by edges
+        */
+        vector<Precinct> precincts;
 
-  // edges between precincts and connecting them
-  vector<vector<int>> edges;
+        // edges between precincts and connecting them
+        vector<vector<int>> edges;
 
-  // write BFS to a file
-  // to find node distance
-  void BFS(int start, int end);
-
+        // write BFS to a file
+        // to find node distance
+        void BFS(int start, int end);
 };
