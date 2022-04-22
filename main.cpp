6914ec55905f81b6f232693ffd608af44c87d399
hhhzzzsss <hyzhou42@gmail.com>
2022-04-22 22:21:00 +0000

    Test code for partition

--- main.cpp
+++ main.cpp
@@ -10,12 +10,5 @@ int main(int argc, char *argv[]) {
     srand(time(NULL));
 
     Graph graph("cdata.txt");
-    cout << graph.getPrecincts()[0].population << endl;
-    cout << graph.getPrecincts()[0].democraticVotes << endl;
-    cout << graph.getPrecincts()[0].republicanVotes << endl;
-    cout << graph.getEdges()[0][0] << endl;
-    cout << graph.BFS(0,13) << endl;
-    cout << graph.BFS(0,2876) << endl;
-    cout << graph.BFS(0,8000) << endl;
     Partition partition(&graph, 10);
 }
