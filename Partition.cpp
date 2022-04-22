786db7e46369fc65a083122f497b36378f5fca0e
hhhzzzsss <hyzhou42@gmail.com>
2022-04-22 06:53:18 +0000

    Added district adjacency handling

--- Partition.cpp
+++ Partition.cpp
@@ -10,12 +10,17 @@ using std::unordered_map;
 typedef std::pair<int, int> ipair;
 
 Partition::Partition(Graph* graph, int numDistricts): graph(graph), numDistricts(numDistricts) {
-    randomJoinInitialize();
-
     // Initial contents of the weights don't matter, only the dimensions of the vectors.
     weights = graph->getEdges();
+
+    randomJoinInitialize();
+    initializeDistrictAdjacencies();
 }
 
+/**
+ * Initializes partition by randomly joining adjacent precincts
+ * until there is the right number of districts
+ */
 void Partition::randomJoinInitialize() {
     int numPrecincts = graph->numPrecincts();
     if (numPrecincts < numDistricts) {
@@ -48,12 +53,61 @@ void Partition::randomJoinInitialize() {
     // collect each the precincts of each district into vectors
     unordered_map<int, int> idxMap;
     int districtIdx = 0;
-    districts.resize(numDistricts);
+    districtToPrecincts.resize(numDistricts);
     for (int i=0; i<numPrecincts; i++) {
         int rootIdx = dsets.find(i);
         if (!idxMap.count(rootIdx)) {
             idxMap[rootIdx] = districtIdx++;
         }
-        districts[idxMap[rootIdx]].push_back(i);
+        precinctToDistrict[i] = idxMap[rootIdx];
+        districtToPrecincts[idxMap[rootIdx]].push_back(i);
     }
+}
+
+// Initialize the district adjacency sets
+void Partition::initializeDistrictAdjacencies() {
+    for (int district = 0; district < numDistricts; district++) {
+        districtAdjacencies[district].clear();
+        for (int precinct : districtToPrecincts[district]) {
+            for (int adjacentPrecinct : graph->getEdges()[precinct]) {
+                int adjacentDistrict = precinctToDistrict[adjacentPrecinct];
+                if (adjacentDistrict != district) {
+                    districtAdjacencies[district].insert(adjacentDistrict);
+                }
+            }
+        }
+    }
+}
+
+// Remove all adjacency relationships for a particular district
+void Partition::removeDistrictAdjacencies(int district) {
+    for (int adjacentDistrict : districtAdjacencies[district]) {
+        districtAdjacencies[adjacentDistrict].erase(district);
+    }
+    districtAdjacencies[district].clear();
+}
+
+// Calculate and add all adjacency relationships for a particular district
+void Partition::addDistrictAdjacencies(int district) {
+    districtAdjacencies[district].clear();
+    for (int precinct : districtToPrecincts[district]) {
+        for (int adjacentPrecinct : graph->getEdges()[precinct]) {
+            int adjacentDistrict = precinctToDistrict[adjacentPrecinct];
+            if (adjacentDistrict != district) {
+                districtAdjacencies[district].insert(adjacentDistrict);
+                districtAdjacencies[adjacentDistrict].insert(district);
+            }
+        }
+    }
+}
+
+// Do a random recombination of two districts
+void Partition::recombination() {
+    // todo
+}
+
+// Do a recombination of two particular districts
+void Partition::recombination(int districtA, int districtB) {
+    removeDistrictAdjacencies(districtA);
+    removeDistrictAdjacencies(districtB);
 }
--- Partition.h
+++ Partition.h
@@ -1,19 +1,29 @@
 #pragma once
 
 #include <vector>
+#include <unordered_set>
 #include "Graph.h"
 
 using std::vector;
+using std::unordered_set;
 class Partition {
     public:
         Partition(Graph* graph, int numDistricts);
-        void randomJoinInitialize();
+        void recombination();
+        void recombination(int districtA, int districtB);
     private:
         Graph* graph;
         int numDistricts;
-        vector<vector<int>> districts;
+        vector<vector<int>> districtToPrecincts;
+        vector<int> precinctToDistrict;
+        vector<unordered_set<int>> districtAdjacencies;
         vector<vector<int>> weights;
-        void recom();
+
+        void randomJoinInitialize();
+        void initializeDistrictAdjacencies();
+        void removeDistrictAdjacencies(int district);
+        void addDistrictAdjacencies(int district);
+
         void minSpanningTree(int district);
         void uniformSpanningTree(int district);
 };
