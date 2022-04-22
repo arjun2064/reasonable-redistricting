26628520d1723b6a7bf4db0150d9707ec8970c89
hhhzzzsss <hyzhou42@gmail.com>
2022-04-22 02:45:30 +0000

    Added constructor to Partition.h and fixed public/private

--- Partition.h
+++ Partition.h
@@ -1,9 +1,18 @@
+#pragma once
+
 #include <vector>
+#include "Graph.h"
 
 using std::vector;
 class Partition {
-    vector<vector<int> > districts;
-    void recom();
-    void minSpanningTree(int district);
-    void uniformSpanningTree(int district);
+    public:
+        Partition(Graph graph, int numDivisions);
+    private:
+        const Graph graph;
+        const int numDivisions;
+        vector<vector<int>> districts;
+        vector<vector<int>> weights;
+        void recom();
+        void minSpanningTree(int district);
+        void uniformSpanningTree(int district);
 };
