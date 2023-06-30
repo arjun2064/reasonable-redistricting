16bc1b3bdcca84dcbcdd6377db846f3772bebf90
Arjun <arjun.arj2064@gmail.com>
2023-06-29 19:47:03 -0600

    adding plots for comparision

--- createPlots.py
+++ createPlots.py
@@ -42,24 +42,24 @@ plt.clf()
 
 exit()
 
-# plot to compare with different existing plans
-# data = list(map(lambda x:float(x), open('./output/mean-median-mst.txt').readlines()))
-# data = list(map(lambda x:float(x), open('./output/mean-median-mst-pa.txt').readlines()))
+plot to compare with different existing plans
+data = list(map(lambda x:float(x), open('./output/mean-median-mst.txt').readlines()))
+data = list(map(lambda x:float(x), open('./output/mean-median-mst-pa.txt').readlines()))
 
-# #create histogram with specific number of bins
-# plt.hist(data, bins=50)
-# plt.title("Histogram of Mean-Median with MST ")
+#create histogram with specific number of bins
+plt.hist(data, bins=50)
+plt.title("Histogram of Mean-Median with MST ")
 
-# _, max_ylim = plt.ylim()
-# with open("./output/PA-mean-median.txt") as f:
-#     for i, line in enumerate(f):
-#         planKey, metric = line.split(" ")
-#         if planKey not in ["GOV", "538DEM", "538GOP", "538CMPCT"]:
-#             continue
-#         metric = float(metric)
-#         plt.axvline(metric, color='orange', linestyle='dashed', linewidth=2)
-#         # plt.axvline(metric, linestyle='dashed', linewidth=2)
-#         plt.text(metric*1.1, max_ylim*(0.9-i*0.1), f"{planKey}")
+_, max_ylim = plt.ylim()
+with open("./output/PA-mean-median.txt") as f:
+    for i, line in enumerate(f):
+        planKey, metric = line.split(" ")
+        if planKey not in ["GOV", "538DEM", "538GOP", "538CMPCT"]:
+            continue
+        metric = float(metric)
+        plt.axvline(metric, color='orange', linestyle='dashed', linewidth=2)
+        # plt.axvline(metric, linestyle='dashed', linewidth=2)
+        plt.text(metric*1.1, max_ylim*(0.9-i*0.1), f"{planKey}")
 
-# plt.savefig('plots/compare.png')
-# plt.clf()
+plt.savefig('plots/compare.png')
+plt.clf()
